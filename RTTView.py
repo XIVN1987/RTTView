47c2ad8d3f168b918ec862fca6a6aa74b4ea0a53
XIVN1987 <XIVN1987@163.com>
2022-01-15 14:54:25 +0800

    add input decode & output encode support

--- RTTView.py
+++ RTTView.py
@@ -1,4 +1,4 @@
-#!python3
+#! python3
 import os
 import sys
 import ctypes
@@ -64,7 +64,6 @@ class RTTView(QWidget):
         self.tmrRTT.start()
 
         self.tmrRTT_Cnt = 0
-        self.tmrDAP_Cnt = 0
     
     def initSetting(self):
         if not os.path.exists('setting.ini'):
@@ -73,14 +72,28 @@ class RTTView(QWidget):
         self.conf = configparser.ConfigParser()
         self.conf.read('setting.ini', encoding='utf-8')
         
-        if not self.conf.has_section('J-Link'):
-            self.conf.add_section('J-Link')
-            self.conf.set('J-Link', 'dllpath', '')
-            self.conf.add_section('Memory')
-            self.conf.set('Memory', 'rttaddr', '0x20000000')
+        if not self.conf.has_section('link'):
+            self.conf.add_section('link')
+            self.conf.set('link', 'jlink', '')
+            self.conf.set('link', 'select', '')
+            self.conf.set('link', 'memory', '0x20000000')
 
-        self.cmbDLL.addItem(self.conf.get('J-Link', 'dllpath'))
-        self.linRTT.setText(self.conf.get('Memory', 'rttaddr'))
+            self.conf.add_section('encode')
+            self.conf.set('encode', 'input', 'ASCII')
+            self.conf.set('encode', 'output', 'ASCII')
+            self.conf.set('encode', 'oenter', r'\r\n')  # output enter (line feed)
+
+        self.cmbDLL.addItem(self.conf.get('link', 'jlink'))
+        self.daplink_detect()    # add DAPLink
+
+        index = self.cmbDLL.findText(self.conf.get('link', 'select'))
+        self.cmbDLL.setCurrentIndex(index if index != -1 else 0)
+
+        self.linRTT.setText(self.conf.get('link', 'memory'))
+
+        self.cmbICode.setCurrentIndex(self.cmbICode.findText(self.conf.get('encode', 'input')))
+        self.cmbOCode.setCurrentIndex(self.cmbOCode.findText(self.conf.get('encode', 'output')))
+        self.cmbEnter.setCurrentIndex(self.cmbEnter.findText(self.conf.get('encode', 'oenter')))
 
     def initQwtPlot(self):
         self.PlotData  = [[0]*1000 for i in range(N_CURVES)]
@@ -93,6 +106,18 @@ class RTTView(QWidget):
         self.vLayout.insertWidget(0, self.ChartView)
         
         self.PlotCurve = [QLineSeries() for i in range(N_CURVES)]
+
+    def daplink_detect(self):
+        try:
+            from pyocd.probe import aggregator
+            self.daplinks = aggregator.DebugProbeAggregator.get_all_connected_probes()
+            if len(self.daplinks) != self.cmbDLL.count() - 1:
+                for i in range(1, self.cmbDLL.count()):
+                    self.cmbDLL.removeItem(i)
+                for i, daplink in enumerate(self.daplinks):
+                    self.cmbDLL.addItem(daplink.product_name)
+        except Exception as e:
+            pass
     
     @pyqtSlot()
     def on_btnOpen_clicked(self):
@@ -197,22 +222,66 @@ class RTTView(QWidget):
         self.xlk.write_U32(self.aDownAddr + 4*3, aDown.WrOff)
     
     def on_tmrRTT_timeout(self):
+        self.tmrRTT_Cnt += 1
         if self.btnOpen.text() == '关闭连接':
             try:
                 self.rcvbuff += self.aUpRead()
                 
                 if self.txtMain.isVisible():
-                    if self.chkHEXShow.isChecked():
-                        text = ''.join(f'{x:02X} ' for x in self.rcvbuff)
+                    text = ''
+                    if self.cmbICode.currentText() == 'ASCII':
+                        text = ''.join([chr(x) for x in self.rcvbuff])
+                        self.rcvbuff = b''
 
-                    else:
-                        text = self.rcvbuff.decode('latin')
+                    elif self.cmbICode.currentText() == 'HEX':
+                        text = ' '.join([f'{x:02X}' for x in self.rcvbuff]) + ' '
+                        self.rcvbuff = b''
 
+                    elif self.cmbICode.currentText() == 'GBK':
+                        while len(self.rcvbuff):
+                            if self.rcvbuff[0:1].decode('GBK', 'ignore'):
+                                text += self.rcvbuff[0:1].decode('GBK')
+                                self.rcvbuff = self.rcvbuff[1:]
+
+                            elif len(self.rcvbuff) > 1 and self.rcvbuff[0:2].decode('GBK', 'ignore'):
+                                text += self.rcvbuff[0:2].decode('GBK')
+                                self.rcvbuff = self.rcvbuff[2:]
+
+                            elif len(self.rcvbuff) > 1:
+                                text += chr(self.rcvbuff[0])
+                                self.rcvbuff = self.rcvbuff[1:]
+
+                            else:
+                                break
+
+                    elif self.cmbICode.currentText() == 'UTF-8':
+                        while len(self.rcvbuff):
+                            if self.rcvbuff[0:1].decode('UTF-8', 'ignore'):
+                                text += self.rcvbuff[0:1].decode('UTF-8')
+                                self.rcvbuff = self.rcvbuff[1:]
+
+                            elif len(self.rcvbuff) > 1 and self.rcvbuff[0:2].decode('UTF-8', 'ignore'):
+                                text += self.rcvbuff[0:2].decode('UTF-8')
+                                self.rcvbuff = self.rcvbuff[2:]
+
+                            elif len(self.rcvbuff) > 2 and self.rcvbuff[0:3].decode('UTF-8', 'ignore'):
+                                text += self.rcvbuff[0:3].decode('UTF-8')
+                                self.rcvbuff = self.rcvbuff[3:]
+
+                            elif len(self.rcvbuff) > 3 and self.rcvbuff[0:4].decode('UTF-8', 'ignore'):
+                                text += self.rcvbuff[0:4].decode('UTF-8')
+                                self.rcvbuff = self.rcvbuff[4:]
+
+                            elif len(self.rcvbuff) > 3:
+                                text += chr(self.rcvbuff[0])
+                                self.rcvbuff = self.rcvbuff[1:]
+
+                            else:
+                                break
+                    
                     if len(self.txtMain.toPlainText()) > 25000: self.txtMain.clear()
                     self.txtMain.moveCursor(QtGui.QTextCursor.End)
                     self.txtMain.insertPlainText(text)
-
-                    self.rcvbuff = b''
 
                 else:
                     if self.rcvbuff.rfind(b',') == -1: return
@@ -230,7 +299,6 @@ class RTTView(QWidget):
                     
                     self.rcvbuff = self.rcvbuff[self.rcvbuff.rfind(b',')+1:]
 
-                    self.tmrRTT_Cnt += 1
                     if self.tmrRTT_Cnt % 4 == 0:
                         if len(d[-1]) != len(self.PlotChart.series()):
                             for series in self.PlotChart.series():
@@ -256,32 +324,28 @@ class RTTView(QWidget):
                 print(str(e))   # 波形显示模式下 txtMain 不可见，因此错误信息不能显示在其上
 
         else:
-            self.tmrDAP_Cnt += 1
-            if self.tmrDAP_Cnt % 100 == 0:
-                try:
-                    from pyocd.probe import aggregator
-                    self.daplinks = aggregator.DebugProbeAggregator.get_all_connected_probes()
-                    if len(self.daplinks) != self.cmbDLL.count() - 1:
-                        for i in range(1, self.cmbDLL.count()):
-                            self.cmbDLL.removeItem(i)
-                        for i, daplink in enumerate(self.daplinks):
-                            self.cmbDLL.addItem(daplink.product_name)
-                except Exception as e:
-                    pass
+            if self.tmrRTT_Cnt % 100 == 1:
+                self.daplink_detect()
 
     @pyqtSlot()
     def on_btnSend_clicked(self):
         if self.btnOpen.text() == '关闭连接':
             text = self.txtSend.toPlainText()
 
-            try:
-                if self.chkHEXSend.isChecked():
-                    text = ''.join([chr(int(x, 16)) for x in text.split()])
+            if self.cmbOCode.currentText() == 'HEX':
+                try:
+                    self.aDownWrite(bytes([int(x, 16) for x in text.split()]))
+                except Exception as e:
+                    print(e)
 
-                self.aDownWrite(text.encode('latin'))
-
-            except Exception as e:
-                self.txtMain.append(f'\n{str(e)}\n')
+            else:
+                if self.cmbEnter.currentText() == r'\r\n':
+                    text = text.replace('\n', '\r\n')
+                
+                try:
+                    self.aDownWrite(text.encode(self.cmbOCode.currentText()))
+                except Exception as e:
+                    print(e)
 
     @pyqtSlot()
     def on_btnDLL_clicked(self):
@@ -290,7 +354,7 @@ class RTTView(QWidget):
             self.cmbDLL.setItemText(0, dllpath)
 
     @pyqtSlot(int)
-    def on_chkWavShow_stateChanged(self, state):
+    def on_chkWave_stateChanged(self, state):
         self.ChartView.setVisible(state == Qt.Checked)
         self.txtMain.setVisible(state == Qt.Unchecked)
 
@@ -299,8 +363,12 @@ class RTTView(QWidget):
         self.txtMain.clear()
     
     def closeEvent(self, evt):
-        self.conf.set('J-Link', 'dllpath', self.cmbDLL.itemText(0))
-        self.conf.set('Memory', 'rttaddr', self.linRTT.text())
+        self.conf.set('link',   'jlink',  self.cmbDLL.itemText(0))
+        self.conf.set('link',   'select', self.cmbDLL.currentText())
+        self.conf.set('link',   'memory', self.linRTT.text())
+        self.conf.set('encode', 'input',  self.cmbICode.currentText())
+        self.conf.set('encode', 'output', self.cmbOCode.currentText())
+        self.conf.set('encode', 'oenter', self.cmbEnter.currentText())
         self.conf.write(open('setting.ini', 'w', encoding='utf-8'))
         
 
@@ -308,4 +376,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     rtt = RTTView()
     rtt.show()
-    app.exec_()
+    app.exec()
