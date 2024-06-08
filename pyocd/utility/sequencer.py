e39977633a629270f2061b0a96582507518095ff
XIVN1987 <XIVN1987@163.com>
2024-06-08 10:57:42 +0800

    from collections.abc import Callable

--- pyocd/utility/sequencer.py
+++ pyocd/utility/sequencer.py
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import (OrderedDict, Callable)
+from collections import OrderedDict
+from collections.abc import Callable
 import logging
 
 log = logging.getLogger("sequencer")
